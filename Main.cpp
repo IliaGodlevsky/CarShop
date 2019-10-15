6c42c5d715e23876533077101120ac5ee086927c
IliaGodlevsky <godleevsky2015@gmail.com>
2019-10-15 23:07:28 +0300

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -2,10 +2,10 @@
 #include "Constants.h"
 #include "AutoShop.h"
 
-int main(int args, char* argv[])
+int main(int argc, char* argv[])
 {
 	size_t shop_size = input(shop_msg,
 		MAX_SHOP_SIZE, MIN_SHOP_SIZE);
-	AutoShop shop(shop_size, plant());
+	AutoShop shop(shop_size, rand_car);
 	visit_auto_shop(shop);
 }
